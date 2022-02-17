26f0385a291be4134859032d04b08b6dca40a5d8
BT_1E10_13 <sample@bths.edu>
2022-02-17 11:01:36 -0500

    Init Commit

--- src/Sort.java
+++ src/Sort.java
@@ -3,13 +3,15 @@ import java.util.ArrayList;
 public class Sort
 {
   // Part A.  Which Sort is Which?
-  public static void firstSortingAlgorithm(int[] elements)
+  public static void selectionSort(int[] elements)
   {
+    int time = 0;
     for (int j = 0; j < elements.length - 1; j++)
     {
       int minIndex = j;
       for (int k = j + 1; k < elements.length; k++)
-      {      
+      {
+        time++;
         if (elements[k] < elements[minIndex])
         {
           minIndex = k;
@@ -19,28 +21,32 @@ public class Sort
       elements[j] = elements[minIndex];
       elements[minIndex] = temp;
     }
+    System.out.println("Run time: "  + time);
   }
 
   // Part A.  Which Sort is Which?
-  public static void secondSortingAlgorithm(int[] elements)
-  { 
+  public static void insertionSort(int[] elements)
+  {
+    int time = 0;
     for (int j = 1; j < elements.length; j++)
     {
       int temp = elements[j];
       int possibleIndex = j;
       while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
-      {     
+      {
+        time++;
         elements[possibleIndex] = elements[possibleIndex - 1];
         possibleIndex--;
       }
       elements[possibleIndex] = temp;
     }
+    System.out.println("Run time: " + time);
   }
   
   // Part C.  Sorting a 1000-word list!
   public static void selectionSortWordList(ArrayList<String> words)
   {
-    /* TO BE IMPLEMENTED IN PART C */
+
   }
 
   // Part C.  Sorting a 1000-word list!
--- src/SortTester.java
+++ src/SortTester.java
@@ -5,12 +5,12 @@ public class SortTester
   public static void main(String[] args)
   {
     /* --- SELECTION SORT --- */
-    int[] arr1 = {3, 86, -20, 14, 40, 19, -4, 100, 89, 13, 54, 12, 77, -5, 0, 6}; 
+    int[] arr1 = {256, 234, 231, 222, 210, 123, 120, 111, 110, 90, 50, 33, 23, 10, 1, -5, -6};
     Sort.selectionSort(arr1);
     System.out.println(Arrays.toString(arr1));
  
     /* --- INSERTION SORT --- */ 
-    int[] arr2 = {3, 86, -20, 14, 40, 19, -4, 100, 89, 13, 54, 12, 77, -5, 0, 6};    
+    int[] arr2 = {256, 234, 231, 222, 210, 123, 120, 111, 110, 90, 50, 33, 23, 10, 1, -5, -6};
     Sort.insertionSort(arr2);
     System.out.println(Arrays.toString(arr2));
   }
